ae2165f03e8daf037f572bef01fab23c0b11a6e4
Francisco Ramos <326franciscorv@gmail.com>
2021-08-17 23:08:09 +0000

    Solve test-lab

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+	if len(os.Args) > 0 {
+		name := strings.Join(os.Args[1:]," ")
+		fmt.Printf("Welcome %v to the jungle \n", name)
+	}
+
 }
