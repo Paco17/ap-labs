8d67b34856c9569cd9ef4ceb3d9b02ac4d9c5c7c
Francisco Ramos <326franciscorv@gmail.com>
2021-10-01 04:58:50 +0000

    str-lib-c

--- labs/c-str-lib/static-linking.c
+++ labs/c-str-lib/static-linking.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+extern int mystrlen(char*);
+extern char* mystradd(char*, char*);
+extern int mystrfind(char*, char*);
+
+int main(int argc, char* argv[]) {
+    if(argc == 4){
+        if(strcmp(argv[0], "-add")){//Funcion add
+             printf("Initial Length: %d\n", mystrlen(argv[2]));
+            char* newString = mystradd(argv[2], argv[3]);
+            printf("New String:  %s\n", newString);
+            printf("New Length: %d\n\n", mystrlen(newString));
+
+        }else if(strcmp(argv[0], "-find")){//Funcion find
+            int p = mystrfind(argv[2], argv[3]);
+            if(p>=0){
+                printf("[%s] string was found at [%d] position\n\n", argv[3], p);
+            }
+            else{
+                printf("Error no hubo ocurrencias\n");
+            }
+           
+        }else{
+            printf("Error en nombrar la funcion. \n Ej: ./main.o -add (or -find) 'Initial String '' 'This is the rest to be added' \n ");
+
+        }
+
+    }else{
+        printf("No hay suficientes parametros. \n Ej: ./main.o -add 'Initial String '' 'This is the rest to be added' \n ");
+    }
     return 0;
 }
--- labs/c-str-lib/strlib.c
+++ labs/c-str-lib/strlib.c
@@ -1,13 +1,44 @@
-
-
 int mystrlen(char *str){
-    return 0;
+    int length = 0;
+    for(int i=0; str[i]!= '\0'; i++){
+        length++;
+    }
+
+    return length;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    char* str[mystrlen(origin)+ mystrlen(addition)];
+    int i= 0, j=0;
+    
+    while(origin[i]!= "/0"){
+        str[i] = origin[i];
+        i++;
+    }
+
+    while(addition[j] != '/0'){
+        str[i] = origin[j];
+        j++;
+    }
+
+    return str;
+
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int pOrg =0, pSubs=0, index = 0, lenOrg = mystrlen(origin);
+    while(origin[pOrg]!='\0'){
+        index = pOrg;
+        while(origin[pOrg]!='\0' && origin[pOrg] == substr[pSubs]){
+            pOrg++;
+            pSubs++;
+        }
+
+        if(lenOrg == pSubs)
+            return index;
+        pSubs = 0;
+        pOrg++;
+    }
+
+    return -1;
 }
